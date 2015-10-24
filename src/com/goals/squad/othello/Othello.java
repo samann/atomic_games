5cbdbfedceeb21965a3e9452cb77b7bd7d2aa4a1
Spencer Amann <amanns@mail.gvsu.edu>
2015-10-24 13:21:28 -0400

    Add some logic for finding valid spots

--- src/com/goals/squad/othello/Othello.java
+++ src/com/goals/squad/othello/Othello.java
@@ -14,7 +14,9 @@ public class Othello {
 
     private static final Logger log = Logger.getLogger(Othello.class.getName());
 
-    private static String color;
+    private static String their_color;
+    private static String our_color;
+    private static final String empty = "-";
     private static int time;
     private static String[] squares;
 
@@ -46,56 +48,312 @@ public class Othello {
         squares = board.squares;
 
         if (args[1].equals("black")) {
-            color = "w"; // looking for white pieces since we are black
+            their_color = "w"; // looking for white pieces since we are black
+            our_color = "b";
         } else {
-            color = "b";
+            their_color = "b";
+            our_color = "w";
         }
         time = Integer.parseInt(args[2]);
     }
 
-    public static boolean[] fill_valid_sqaures() {
-        boolean[] valid = new boolean[squares.length];
-        int index = -1;
-        Arrays.fill(valid, false);
-        for (int i = 0; i < squares.length; i++) {
-            if (squares[i].equals(color)) {
-                if (index % 8 > 0 || index % 8 < 7) { // check the sides
-                    if ((index = check_vert(i)) > 0) {
-
-                    }
-                }
-                if ((index > 0 && index < 7) || (index < 63 && index > 56)) { // top and bottom
-                    if ((index = check_horz(i)) > 0) {
-
-                    }
-                } else {
-                    if ((index = check_horz(i)) > 0) {
-                        valid[index] = true;
-                    }
-                    if ((index = check_vert(i)) > 0) {
-                        valid[index] = true;
-                    }
-                }
-
-            }
-        }
-
-        return valid;
-    }
-
+//    public static boolean[] fill_valid_sqaures() {
+//        boolean[] valid = new boolean[squares.length];
+//        int index = -1;
+//        Arrays.fill(valid, false);
+//        for (int i = 1; i < squares.length - 1; i++) {
+//            // find a vlid spot based on one square
+//            if (squares[i].equals(their_color)) {
+//                if (index % 8 > 0 || index % 8 < 7) { // check the sides
+//                    if ((index = check_vert(i)) > 0) {
+//
+//                    }
+//                }
+//                if ((index > 0 && index < 7) || (index < 63 && index > 56)) { // top and bottom
+//                    if ((index = check_horz(i)) > 0) {
+//
+//                    }
+//                } else {
+//                    if ((index = check_horz(i)) > 0) {
+//                        valid[index] = true;
+//                    }
+//                    if ((index = check_vert(i)) > 0) {
+//                        valid[index] = true;
+//                    }
+//                }
+//
+//            }
+//        }
+//
+//        return valid;
+//    }
+    /**
+     * if our color is on one side and empty onthe other
+     */
     public static int check_horz(int index) {
-     
+        if (squares[index + 1].equals(our_color) && squares[index - 1].equals(empty)) {
+            return index - 1;
+        }
+        if (squares[index - 1].equals(our_color) && squares[index + 1].equals(empty)) {
+            return index + 1;
+        }
+        return -1;
     }
 
     public static int check_vert(int index) {
-
+        if (squares[index + 8].equals(our_color) && squares[index - 8].equals(empty)) {
+            return index - 8;
+        }
+        if (squares[index - 8].equals(our_color) && squares[index + 8].equals(empty)) {
+            return index + 8;
+        }
+        return -1;
     }
 
     public static int check_diag_lr(int index) {
 
+        return -1;
     }
 
     public static int check_diag_rl(int index) {
 
+        return -1;
+    }
+    public static boolean[] fill_valid_sqaures() {
+        boolean[] valid = new boolean[squares.length];
+        int index = -1;
+        Arrays.fill(valid, false);
+        for (int i = 0; i < squares.length - 1; i++) {
+            valid[i] = is_valid(i);
+        }
+        return valid;
+    }
+
+    public static boolean traverse(int index, int dir) {
+        while ((index > 0 && index < 63) ||
+                ( (index % 8 != 7) || (index % 8 != 0) ) &&
+                        squares[index + dir].equals(their_color)) {
+            if (squares[index + dir].equals(our_color) ){
+                return true;
+            }
+            if (squares[index + dir].equals(empty)) {
+                return false;
+            }
+            index = index + dir;
+        }
+        return false;
+    }
+
+    public static boolean is_valid(int index) {
+        if (squares[index].equals(empty)) {
+            // middle of board
+            if (index % 8 > 0 || index % 8 < 7) {
+
+                // below
+                if (squares[index + 8].equals(their_color)) {
+                    return traverse(index, 8);
+                }
+
+                // above
+                if (squares[index - 8].equals(their_color)) {
+                    return traverse(index, -8);
+                }
+
+                // right
+                if (squares[index + 1].equals(their_color)) {
+                    return traverse(index, 1);
+                }
+
+                // left
+                if (squares[index - 1].equals(their_color)) {
+                    return traverse(index, -1);
+                }
+
+                // up right
+                if (squares[index - 7].equals(their_color)) {
+                    return traverse(index, -7);
+                }
+
+                // up left
+                if (squares[index - 9].equals(their_color)) {
+                    return traverse(index, -9);
+                }
+
+                // below left
+                if (squares[index + 7].equals(their_color)) {
+                    return traverse(index, 7);
+                }
+
+                // below right
+                if (squares[index + 9].equals(their_color)) {
+                    return traverse(index, 9);
+                }
+                // top row
+            } else if (index > 0 && index < 7) {
+
+                // below left
+                if (squares[index + 7].equals(their_color)) {
+                    return traverse(index, 7);
+                }
+
+                // below right
+                if (squares[index + 9].equals(their_color)) {
+                    return traverse(index, 9);
+                }
+
+                // right
+                if (squares[index + 1].equals(their_color)) {
+                    return traverse(index, 1);
+                }
+
+                // left
+                if (squares[index - 1].equals(their_color)) {
+                    return traverse(index, -1);
+                }
+
+                // below
+                if (squares[index + 8].equals(their_color)) {
+                    return traverse(index, 8);
+                }
+                // bottom row
+            } else if (index > 56 && index < 63) {
+                // above
+                if (squares[index - 8].equals(their_color)) {
+                    return traverse(index, -8);
+                }
+
+                // right
+                if (squares[index + 1].equals(their_color)) {
+                    return traverse(index, 1);
+                }
+
+                // left
+                if (squares[index - 1].equals(their_color)) {
+                    return traverse(index, -1);
+                }
+
+                // up right
+                if (squares[index - 7].equals(their_color)) {
+                    return traverse(index, -7);
+                }
+
+                // up left
+                if (squares[index - 9].equals(their_color)) {
+                    return traverse(index, -9);
+                }
+                // left column
+            } else if (index % 8 == 0) {
+                if (index == 0) {
+                    // below right
+                    if (squares[index + 9].equals(their_color)) {
+                        return traverse(index, 9);
+                    }
+
+                    // below
+                    if (squares[index + 8].equals(their_color)) {
+                        return traverse(index, 8);
+                    }
+
+                    // right
+                    if (squares[index + 1].equals(their_color)) {
+                        return traverse(index, 1);
+                    }
+                } else if (index == 56){
+                    // up right
+                    if (squares[index - 7].equals(their_color)) {
+                        return traverse(index, -7);
+                    }
+                    // above
+                    if (squares[index - 8].equals(their_color)) {
+                        return traverse(index, -8);
+                    }
+
+                    // right
+                    if (squares[index + 1].equals(their_color)) {
+                        return traverse(index, 1);
+                    }
+                } else {
+                    // up right
+                    if (squares[index - 7].equals(their_color)) {
+                        return traverse(index, -7);
+                    }
+
+                    // below right
+                    if (squares[index + 9].equals(their_color)) {
+                        return traverse(index, 9);
+                    }
+
+                    // below
+                    if (squares[index + 8].equals(their_color)) {
+                        return traverse(index, 8);
+                    }
+
+                    // above
+                    if (squares[index - 8].equals(their_color)) {
+                        return traverse(index, -8);
+                    }
+
+                    // right
+                    if (squares[index + 1].equals(their_color)) {
+                        return traverse(index, 1);
+                    }
+                }
+            } else if (index % 8 == 7) {
+                if (index == 63) {
+                    // above
+                    if (squares[index - 8].equals(their_color)) {
+                        return traverse(index, -8);
+                    }
+                    // left
+                    if (squares[index - 1].equals(their_color)) {
+                        return traverse(index, -1);
+                    }
+                    // up left
+                    if (squares[index - 9].equals(their_color)) {
+                        return traverse(index, -9);
+                    }
+                }
+                else if (index == 7) {
+                    // below
+                    if (squares[index + 8].equals(their_color)) {
+                        return traverse(index, 8);
+                    }
+                    // below left
+                    if (squares[index + 7].equals(their_color)) {
+                        return traverse(index, 7);
+                    }
+                    // left
+                    if (squares[index - 1].equals(their_color)) {
+                        return traverse(index, -1);
+                    }
+                } else {
+                    // below
+                    if (squares[index + 8].equals(their_color)) {
+                        return traverse(index, 8);
+                    }
+
+                    // above
+                    if (squares[index - 8].equals(their_color)) {
+                        return traverse(index, -8);
+                    }
+
+                    // up left
+                    if (squares[index - 9].equals(their_color)) {
+                        return traverse(index, -9);
+                    }
+
+                    // below left
+                    if (squares[index + 7].equals(their_color)) {
+                        return traverse(index, 7);
+                    }
+
+                    // left
+                    if (squares[index - 1].equals(their_color)) {
+                        return traverse(index, -1);
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
